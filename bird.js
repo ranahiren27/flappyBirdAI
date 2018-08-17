8e1cec594f46a07d01e3088c34c31c3d8fc18a09
ranahiren27 <ranahiren27@gmail.com>
2018-08-17 21:42:10 +0530

    first commit

---
+++ bird.js
@@ -0,0 +1,104 @@
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+// Code for: https://youtu.be/cXgA1d_E-jY
+
+// Class is exported (eslint flag)
+/* exported Bird */
+function MyMutate(x) {
+  if (random(1) < 0.1) {
+    let offset = randomGaussian() * 0.5;
+    let newx = x + offset;
+    return newx;
+  } else {
+    return x;
+  }
+}
+
+class Bird {
+  constructor(brain) {
+    this.y = height / 2;
+    this.x = 64;
+
+    this.gravity = 0.6;
+    this.lift = -10;
+    this.velocity = 0;
+
+    this.icon = birdSprite;
+    this.width = 64;
+    this.height = 64;
+    // neuroevolution stuff
+    this.score = 0;
+    this.fitness = 0;
+    if(brain){
+      this.brain = brain.copy();
+    }else{
+      this.brain = new NeuralNetwork(5, 7, 1);
+    }
+  }
+
+  show() {
+    // draw the icon CENTERED around the X and Y coords of the bird object
+    image(this.icon, this.x - this.width / 2, this.y - this.height / 2, this.width, this.height);
+  }
+
+  up() {
+    this.velocity = this.lift;
+  }
+
+  mutate(){
+    this.brain.mutate(MyMutate);
+  }
+
+
+  think(pipes) {
+    // First find the closest pipe
+    let closest = null;
+    let record = Infinity;
+    for (let i = 0; i < pipes.length; i++) {
+      let diff = pipes[i].x - this.x;
+      if (diff > 0 && diff < record) {
+        record = diff;
+        closest = pipes[i];
+      }
+    }
+
+    if (closest != null) {
+      // Now create the inputs to the neural network
+      let inputs = [];
+      // x position of closest pipe
+      inputs[0] = map(closest.x, this.x, width, 0, 1);
+      // top of closest pipe opening
+      inputs[1] = map(closest.top, 0, height, 0, 1);
+      // bottom of closest pipe opening
+      inputs[2] = map(closest.bottom, 0, height, 0, 1);
+      // bird's y position
+      inputs[3] = map(this.y, 0, height, 0, 1);
+      // bird's y velocity
+      inputs[4] = map(this.velocity, -5, 5, 0, 1);
+
+      // Get the outputs from the network
+      let action = this.brain.predict(inputs);
+      // Decide to jump or not!
+      if (action[0] > 0.5) {
+        this.up();
+      }
+    }
+}
+
+  update() {
+    this.score++;
+    this.velocity += this.gravity;
+    this.y += this.velocity;
+
+    if (this.y >= height - this.height / 2) {
+      this.y = height - this.height / 2;
+      this.velocity = 0;
+    }
+
+    if (this.y <= this.height / 2) {
+      this.y = this.height / 2;
+      this.velocity = 0;
+    }
+  }
+}
